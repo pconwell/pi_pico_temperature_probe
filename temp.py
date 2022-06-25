d8367bf31cecee6e533ba5d3f7b6b844c65ec1fc
Patrick Conwell <patrick.conwell@gmail.com>
2022-06-25 13:39:07 -0500

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,16 +1,27 @@
-# micropython
-
-from machine import Pin, ADC
+import machine
 import utime
 from math import log
 
 b25 = 3950
 resolution = 65535
+tpins = [26,27,4]
 
-sensor_pin = machine.ADC(0)
+led = machine.Pin(25, machine.Pin.OUT)
 
 while True:
-    reading = sensor_pin.read_u16()
-    ts_C = -273.15 + 1/(0.003354 + log(reading / (resolution - reading))/b25)
-    print(f"temp: {ts_C}")    
-    utime.sleep(2)
+    for i in tpins:
+        
+        reading = machine.ADC(i).read_u16()
+        
+        if i == 4:
+            ts_C = 27 - (((reading * (3.3/resolution)) -0.706)/0.001721)
+            print(f"temp {i}: {ts_C}")
+        else:
+#            log_r = log(reading / (resolution - reading))
+#            ts_C = -273.15 + 1/(1/298.15 + log_r/b25)
+            ts_C = -273.15 + 1/(0.003354 + log(reading / (resolution - reading))/b25)
+            print(f"temp {i}: {ts_C}")
+        led.toggle()
+        utime.sleep(1)
+    
+    print("\n")
