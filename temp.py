4ec9ee758ce666a147339a0a3ee6a91c99e08812
Patrick Conwell <patrick.conwell@gmail.com>
2022-06-25 16:27:50 -0500

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,4 +1,4 @@
-import machine
+from machine import ADC, UART, Pin
 import utime
 from math import log
 
@@ -6,22 +6,25 @@ b25 = 3950
 resolution = 65535
 tpins = [26,27,4]
 
-led = machine.Pin(25, machine.Pin.OUT)
+led = Pin(25, Pin.OUT)
+uart = UART(0, baudrate=9600, tx=Pin(0), rx=Pin(1), bits=8, parity=None, stop=1)
 
 while True:
+    t = {}
     for i in tpins:
         
-        reading = machine.ADC(i).read_u16()
+        reading = ADC(i).read_u16()
         
         if i == 4:
             ts_C = 27 - (((reading * (3.3/resolution)) -0.706)/0.001721)
-            print(f"temp {i}: {ts_C}")
+            t[i] = ts_C
+            #print(f"temp {i}: {ts_C}")
         else:
-#            log_r = log(reading / (resolution - reading))
-#            ts_C = -273.15 + 1/(1/298.15 + log_r/b25)
             ts_C = -273.15 + 1/(0.003354 + log(reading / (resolution - reading))/b25)
-            print(f"temp {i}: {ts_C}")
-        led.toggle()
-        utime.sleep(1)
-    
-    print("\n")
+            #print(f"temp {i}: {ts_C}")
+            t[i] = ts_C
+
+    print(t)
+    uart.write(f"{t}\n")
+    led.toggle()
+    utime.sleep(1)
