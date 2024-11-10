83affca1003b6dce8ecddc7ef829cdfa987c9f57
Patrick Conwell <patrick.conwell@gmail.com>
2024-11-10 13:19:04 -0600

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -3,8 +3,16 @@ import utime
 from math import log
 
 
-b25 = 3950 #resistor beta
-resolution = 65535 #microcontroller resolution (pico 65535, arduino 1023)
+# two-point calibration factor
+a = 1.2
+b = -15.2
+
+BETA = 3950  # Beta value from thermistor specs
+R0 = 10000   # Resistance at 25 degrees C (in ohms)
+T0 = 298.15  # Reference temperature in Kelvin (25 degrees C)
+VCC = 3.3    # Voltage of the system (e.g., 3.3V on the Pico)
+R_FIXED = 10000  # Fixed resistor value (10K Ohm)
+ADC_MAX = 65535  # ADC resolution (16-bit on Pico)
 
 
 def internal(pin):
@@ -13,7 +21,7 @@ def internal(pin):
     reading = ADC(pin).read_u16()
     ts_C = 27 - ((reading * (3.3 / (65535))) - 0.706)/0.001721
     
-    return(ts_C)
+    return round(ts_C,1)
 
 def external(pin):
     # 10k thermistor
@@ -21,4 +29,11 @@ def external(pin):
     reading = ADC(pin).read_u16()
     ts_C = -273.15 + 1/(0.003354 + log(reading / (65535 - reading))/3950)
     
-    return(ts_C)
+    return round(a * ts_C + b,1)
+
+
+if __name__ == "__main__":
+    
+    print(internal(4))
+    print(external(26))
+    print(external(27))
